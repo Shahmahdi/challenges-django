7f16da9bec142dbe8b48bc92ac50e744291a6ce9
Shahmahdi <shahmahdi11@gmail.com>
2026-06-23 02:15:34 +0600

    redirect to another route

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 monthly_challenges = {
   "january": "Eat no meat on January",
@@ -17,7 +17,13 @@ monthly_challenges = {
 }
 
 def Monthly_challenge_by_number(request, month):
-  return HttpResponse(month)
+  months = list(monthly_challenges.keys())
+  
+  if month > len(months):
+    return HttpResponseNotFound("Invalid month")
+  
+  redirect_month = months[month - 1]
+  return HttpResponseRedirect("/challenges/" + redirect_month)
 
 def Monthly_challenge(request, month):
   # challenge_text = None
